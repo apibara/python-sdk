cbb03373f97c783e19364bbf72bf85a8203833cf
Francesco Ceccon <francesco@ceccon.me>
2022-09-13 11:14:10 +0100

    feat: support new stream protocol

--- src/apibara/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.3.0"
-
-from .client import Client
-from .indexer.indexer import IndexerClient, IndexerStream
-from .indexer.runner import IndexerRunner, Info
-from .model import Indexer, IndexerConnected, NewBlock, NewEvents, Reorg
--- src/apibara/legacy/storage.py
+++ src/apibara/legacy/storage.py
@@ -2,8 +2,8 @@ from contextlib import contextmanager
 from typing import Any, Iterable, Iterator, Optional
 
 from pymongo import MongoClient
-import pymongo
 from pymongo.database import Database
+from pymongo.client_session import ClientSession
 
 Document = dict[str, Any]
 Filter = dict[str, Any]
@@ -19,49 +19,64 @@ class IndexerStorage:
             raise ValueError("Storage url must be not None")
 
         self.db_name = indexer_id.replace("-", "_")
+        self._indexer_id = indexer_id
 
         self._mongo = MongoClient(url)
-        self._db = self._mongo[self.db_name]
+        self.db = self._mongo[self.db_name]
 
     @contextmanager
     def create_storage_for_block(self, block_number: int) -> Iterator["Storage"]:
-        yield Storage(self._db, block_number)
+        with self._mongo.start_session() as session:
+            yield Storage(self.db, session, block_number)
+            self._update_indexed_to(block_number, session)
+
+    def starting_sequence(self):
+        state = self.db["_apibara"].find_one({"indexer_id": self._indexer_id})
+        if state is None:
+            return None
+        return state["indexed_to"] + 1
+
+    def _update_indexed_to(self, block_number: int, session: ClientSession):
+        self.db["_apibara"].update_one(
+            {"indexer_id": self._indexer_id},
+            {"$set": {"indexed_to": block_number}},
+            upsert=True,
+            session=session
+        )
 
 
 class Storage:
-    # NOTE: all methods are async even though it's using the sync mongo client.
-    # In the near future the API will connect to mongo through Apibara Server and
-    # so it will be async.
     """Chain-aware document storage."""
 
-    def __init__(self, db: Database, block_number: int) -> None:
+    def __init__(self, db: Database, session: ClientSession, block_number: int) -> None:
         self._db = db
         self._block_number = block_number
+        self._session = session
 
     async def insert_one(self, collection: str, doc: Document):
         self._add_chain_information(doc)
-        self._db[collection].insert_one(doc)
+        self._db[collection].insert_one(doc, session=self._session)
 
     async def insert_many(self, collection: str, docs: Iterable[Document]):
         for doc in docs:
             self._add_chain_information(doc)
-        self._db[collection].insert_many(docs)
+        self._db[collection].insert_many(docs, session=self._session)
 
     async def delete_one(self, collection: str, filter: Filter):
         self._add_current_block_to_filter(filter)
         self._db[collection].update_one(
-            filter, {"$set": {"_chain.valid_to": self._block_number}}
+            filter, {"$set": {"_chain.valid_to": self._block_number}}, session=self._session
         )
 
     async def delete_many(self, collection: str, filter: Filter):
         self._add_current_block_to_filter(filter)
         self._db[collection].update_many(
-            filter, {"$set": {"_chain.valid_to": self._block_number}}
+            filter, {"$set": {"_chain.valid_to": self._block_number}}, session=self._session
         )
 
     async def find_one(self, collection: str, filter: Filter) -> Optional[Document]:
         self._add_current_block_to_filter(filter)
-        return self._db[collection].find_one(filter)
+        return self._db[collection].find_one(filter, session=self._session)
 
     async def find(
         self,
@@ -73,7 +88,7 @@ class Storage:
         limit: int = 0,
     ) -> Iterable[dict]:
         self._add_current_block_to_filter(filter)
-        cursor = self._db[collection].find(filter, projection, skip, limit)
+        cursor = self._db[collection].find(filter, projection, skip, limit, session=self._session)
         if sort is not None:
             for field, order in sort.items():
                 cursor = cursor.sort(field, order)
@@ -89,7 +104,7 @@ class Storage:
         # Step 1. Update the old document (if any) by clamping its validity range
         self._add_current_block_to_filter(filter)
         existing = self._db[collection].find_one_and_update(
-            filter, {"$set": {"_chain.valid_to": self._block_number}}
+            filter, {"$set": {"_chain.valid_to": self._block_number}}, session=self._session
         )
 
         # Step 2. Insert the new document.
@@ -105,7 +120,7 @@ class Storage:
         # Step 1. Update the old document (if any) by clamping its validity range
         self._add_current_block_to_filter(filter)
         existing = self._db[collection].find_one_and_update(
-            filter, {"$set": {"_chain.valid_to": self._block_number}}
+            filter, {"$set": {"_chain.valid_to": self._block_number}}, session=self._session
         )
 
         # Step 2. To simulate an update, first insert then call update on it.
@@ -113,7 +128,7 @@ class Storage:
             del existing["_id"]
             del existing["_chain"]
             await self.insert_one(collection, existing)
-            self._db[collection].update_one(filter, update)
+            self._db[collection].update_one(filter, update, session=self._session)
 
         return existing
 
