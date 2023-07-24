9acbbf246beefb832f8d26006046ec5b960b3ce7
Francesco Ceccon <francesco@ceccon.me>
2023-07-24 15:54:22 +0200

    test: skip tests that require docker

--- test/indexer/test_storage.py
+++ test/indexer/test_storage.py
@@ -7,6 +7,7 @@ from apibara.starknet import starknet_cursor
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="Docker permission in CI")
 async def test_insert_one(storage: IndexerStorage):
     with storage.create_storage_for_block(starknet_cursor(100)) as s:
         await s.insert_one("capibaras", {"name": "bob", "age": 3})
@@ -18,6 +19,7 @@ async def test_insert_one(storage: IndexerStorage):
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="Docker permission in CI")
 async def test_insert_many(storage: IndexerStorage):
     with storage.create_storage_for_block(starknet_cursor(100)) as s:
         await s.insert_many(
@@ -35,6 +37,7 @@ async def test_insert_many(storage: IndexerStorage):
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="Docker permission in CI")
 async def test_delete_one(storage: IndexerStorage):
     with storage.create_storage_for_block(starknet_cursor(100)) as s:
         await s.insert_many(
@@ -55,6 +58,7 @@ async def test_delete_one(storage: IndexerStorage):
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="Docker permission in CI")
 async def test_delete_many(storage: IndexerStorage):
     with storage.create_storage_for_block(starknet_cursor(100)) as s:
         await s.insert_many(
@@ -76,6 +80,7 @@ async def test_delete_many(storage: IndexerStorage):
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="Docker permission in CI")
 async def test_find_one_and_replace(storage: IndexerStorage):
     with storage.create_storage_for_block(starknet_cursor(100)) as s:
         await s.insert_many(
@@ -98,6 +103,7 @@ async def test_find_one_and_replace(storage: IndexerStorage):
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="Docker permission in CI")
 async def test_find_one_and_update(storage: IndexerStorage):
     with storage.create_storage_for_block(starknet_cursor(100)) as s:
         await s.insert_many(
@@ -118,6 +124,7 @@ async def test_find_one_and_update(storage: IndexerStorage):
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="Docker permission in CI")
 async def test_invalidate(storage: IndexerStorage):
     with storage.create_storage_for_block(starknet_cursor(100)) as s:
         await s.insert_many(
